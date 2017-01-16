f8218ada9ff5f03f0fba996c5eab8a4a8f588677
shevchenk <jorgeshevchenk@gmail.com>
2017-01-16 05:06:37 -0500

    actualizacion casi final

--- laravel/app/controllers/ReporteController.php
+++ laravel/app/controllers/ReporteController.php
@@ -191,7 +191,6 @@ class ReporteController extends BaseController
         }
 
         $datos['sede'] = ($datos['sede']=='') ? array('0') : $datos['sede'] ;
-        $datos['tipo'] = ($datos['tipo']=='') ? array('0') : $datos['tipo'] ;
 
         $problemas = Problema::ListadoProblema2($datos);
         /***********************************************************************/
@@ -201,7 +200,7 @@ class ReporteController extends BaseController
         'BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BW','BX','BY','BZ','CA','CB','CC','CD','CE','CF',
         'CG','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CW','CX','CY','CZ','DA','DB','DC','DD','DE','DF','DG',
         'DH','DI','DJ','DK','DL','DM','DN','DO','DP','DQ','DR','DS','DT','DU','DV','DW','DX','DY','DZ');
-        $azcount=array(5,17,21,40,12,18.5,18.5,18.5,40,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
+        $azcount=array(5,17,21,40,12,18.5,18.5,18.5,18.5,40,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
             15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
             15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
             15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
@@ -225,12 +224,34 @@ class ReporteController extends BaseController
         $objPHPExcel->getActiveSheet()->getStyle('A2')->getFont()->setSize(20);
         $objPHPExcel->getActiveSheet()->mergeCells('A2:M2');
         $objPHPExcel->getActiveSheet()->getStyle('A2:M2')->applyFromArray( Config::get("excel.styleAlignmentBold") );
+        $cabecera=array('');
+        if( $datos['tipo']=='' ){
 
-        $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Fecha Estado','Resultado',
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado',
                             'Paterno','Materno','Nombre','Email','Telefono','Carrera','Tipo Carrera','Ciclo','Documento','Observación',
                             'Curso Nota','Frencuencia','Profesor','Fecha Inicio','Fecha Fin','Nota',
                             'Curso Pago','Recibo','Monto'
                         );
+        }
+        elseif( $datos['tipo']=='5' ){
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado');
+        }
+        elseif( $datos['tipo']=='4' ){
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado',
+                            'Paterno','Materno','Nombre','Email','Telefono','Carrera','Tipo Carrera','Ciclo','Documento','Observación',
+                            'Curso Nota','Frencuencia','Profesor','Fecha Inicio','Fecha Fin','Nota',
+                            'Curso Pago','Recibo','Monto'
+                        );
+        }
+        elseif( $datos['tipo']=='3' ){
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado','Tipo Artículo','Artículo','Descripción','Cantidad');
+        }
+        elseif( $datos['tipo']=='2' ){
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado');
+        }
+        elseif( $datos['tipo']=='1' ){
+            $cabecera=array('N°','Sede','Tipo Problema','Descripción','Estado Problema','Fecha Problema','Fecha Registro','Tiempo Transcurrido','Fecha Estado','Resultado');
+        }
 
         for($i=0;$i<count($cabecera);$i++){
         $objPHPExcel->getActiveSheet()->setCellValue($az[$i]."4",$cabecera[$i]);
@@ -258,6 +279,17 @@ class ReporteController extends BaseController
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->paterno);$azpos++;
@@ -306,7 +338,7 @@ class ReporteController extends BaseController
                 }
                 
             }
-            if( $datos['tipo']=='5' ){
+            elseif( $datos['tipo']=='5' ){
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$cont);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->sede);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_problema);$azpos++;
@@ -314,6 +346,153 @@ class ReporteController extends BaseController
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
+            }
+            elseif( $datos['tipo']=='4' ){
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$cont);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->sede);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->descripcion);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->paterno);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->materno);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->nombre);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->email);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->telefono);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->carrera);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_carrera);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->ciclo);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->documento);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->observacion);$azpos++;
+
+                $nota=explode('**',$r->nota);
+                $detallenota=array();
+                for( $i=0;$i<count($nota);$i++ ){
+                    $dnota=explode("|",$nota[$i]);
+                    for( $j=0;$j<count($dnota);$j++ ){
+                        if($i==0){
+                            $detallenota[]='';
+                        }
+                        $detallenota[$j].=$dnota[$j]."\n";
+                    }
+                }
+
+                for( $i=0;$i<count($detallenota);$i++ ){
+                    $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$detallenota[$i]);
+                    $objPHPExcel->getActiveSheet()->getStyle($az[$azpos].$valorinicial)->getAlignment()->setWrapText(true);$azpos++;
+                }
+
+                $pago=explode('**',$r->pago);
+                $detallepago=array();
+                for( $i=0;$i<count($pago);$i++ ){
+                    $dpago=explode("|",$pago[$i]);
+                    for( $j=0;$j<count($dpago);$j++ ){
+                        if($i==0){
+                            $detallepago[]='';
+                        }
+                        $detallepago[$j].=$dpago[$j]."\n";
+                    }
+                }
+
+                for( $i=0;$i<count($detallepago);$i++ ){
+                    $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$detallepago[$i]);
+                    $objPHPExcel->getActiveSheet()->getStyle($az[$azpos].$valorinicial)->getAlignment()->setWrapText(true);$azpos++;
+                }
+            }
+            elseif( $datos['tipo']=='3' ){
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$cont);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->sede);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->descripcion);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_articulo);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->articulo);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->art_descripcion);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->art_cantidad);$azpos++;
+            }
+            elseif( $datos['tipo']=='2' ){
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$cont);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->sede);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->descripcion);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
+            }
+            elseif( $datos['tipo']=='1' ){
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$cont);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->sede);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->tipo_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->descripcion);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->estado_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_problema);$azpos++;
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_registro);$azpos++;
+                $tt="";
+                if($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7==0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem";
+                }
+                elseif($r->tiempo_transcurrido>7 AND $r->tiempo_transcurrido%7!=0){
+                    $tt=floor($r->tiempo_transcurrido/7)." Sem y ".($r->tiempo_transcurrido%7)." Dia(s)";
+                }
+                else{
+                    $tt=($r->tiempo_transcurrido&7)."Dia(s)";
+                }
+                $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$tt);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->fecha_estado);$azpos++;
                 $objPHPExcel->getActiveSheet()->setCellValue($az[$azpos].$valorinicial,$r->resultado);$azpos++;
             }
@@ -323,7 +502,7 @@ class ReporteController extends BaseController
             }
         }
 
-        $objPHPExcel->getActiveSheet()->getStyle('A4:'.$az[$azposfin].$valorinicial)->applyFromArray( Config::get("excel.styleThinBlackBorderAllborders") );
+        $objPHPExcel->getActiveSheet()->getStyle('A4:'.$az[count($cabecera)-1].$valorinicial)->applyFromArray( Config::get("excel.styleThinBlackBorderAllborders") );
         ////////////////////////////////////////////////////////////////////////////////////////////////
         $objPHPExcel->getActiveSheet()->setTitle('Reporte_Problemas');
         $objPHPExcel->setActiveSheetIndex(0);
--- laravel/app/models/Problema.php
+++ laravel/app/models/Problema.php
@@ -104,7 +104,7 @@ protected $table = '';
                 a.paterno,a.materno,a.nombre,a.email,a.telefono,
                 IFNULL(c.nombre,ap.carrera) carrera,IFNULL(tc.nombre,'') tipo_carrera,IFNULL(ci.nombre,'') ciclo,
                 ap.documento,ap.observacion,
-                                apnf.nota,appf.pago
+                apnf.nota,appf.pago,DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
                 FROM problemas p
                 JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
                 JOIN sedes s ON p.sede_id=s.id
@@ -142,8 +142,119 @@ protected $table = '';
                 descripcion, ep.nombre AS estado_problema, 
                 p.fecha_problema,
                 p.created_at AS fecha_registro,
+                pd.fecha_estado, pd.resultado,DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
+                FROM problemas p
+                JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
+                JOIN sedes s ON p.sede_id=s.id
+                JOIN problema_detalle pd ON p.id=pd.problema_id
+                JOIN (SELECT MAX(id) AS id
+                            FROM problema_detalle
+                            WHERE estado=1
+                            GROUP BY problema_id) pd2
+                ON pd.id=pd2.id
+                JOIN estado_problema ep ON pd.estado_problema_id=ep.id
+                WHERE p.estado=1 AND pd.estado=1 
+                AND p.sede_id in ($sede)
+                $tipo
+                $estado $fecha";
+        }
+        elseif( $datos['tipo']=='4' ){
+        $sql="  SELECT p.id, s.nombre AS sede, tp.nombre AS tipo_problema,
+                descripcion, ep.nombre AS estado_problema, 
+                p.fecha_problema,
+                p.created_at AS fecha_registro,
                 pd.fecha_estado, pd.resultado,
-                
+                a.paterno,a.materno,a.nombre,a.email,a.telefono,
+                IFNULL(c.nombre,ap.carrera) carrera,IFNULL(tc.nombre,'') tipo_carrera,IFNULL(ci.nombre,'') ciclo,
+                ap.documento,ap.observacion,
+                apnf.nota,appf.pago,DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
+                FROM problemas p
+                JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
+                JOIN sedes s ON p.sede_id=s.id
+                JOIN problema_detalle pd ON p.id=pd.problema_id
+                JOIN (SELECT MAX(id) AS id
+                            FROM problema_detalle
+                            WHERE estado=1
+                            GROUP BY problema_id) pd2
+                ON pd.id=pd2.id
+                JOIN estado_problema ep ON pd.estado_problema_id=ep.id
+                LEFT JOIN alumno_problema ap ON p.id=ap.problema_id
+                LEFT JOIN alumnos a ON ap.alumno_id=a.id
+                                LEFT JOIN ciclos ci ON ci.id=ap.ciclo_id
+                LEFT JOIN carreras c ON ap.carrera_id = c.id
+                LEFT JOIN tipo_carrera tc ON c.tipo_carrera_id=tc.id
+                LEFT JOIN (
+                    SELECT apn.alumno_problema_id,GROUP_CONCAT( CONCAT_WS('|',apn.curso,apn.frecuencia,apn.profesor,apn.fecha_inicio,apn.fecha_fin,apn.nota) SEPARATOR '**') nota
+                    FROM alumno_problema_nota apn
+                    WHERE apn.estado=1
+                    GROUP BY apn.alumno_problema_id
+                ) apnf ON apnf.alumno_problema_id=ap.id
+                LEFT JOIN (
+                    SELECT app.alumno_problema_id,GROUP_CONCAT( CONCAT_WS('|',app.curso,app.recibo,app.monto) SEPARATOR '**') pago
+                    FROM alumno_problema_pago app
+                    WHERE app.estado=1
+                    GROUP BY app.alumno_problema_id
+                ) appf ON appf.alumno_problema_id=ap.id
+                WHERE p.estado=1 AND pd.estado=1 
+                AND p.sede_id in ($sede)
+                $tipo
+                $estado $fecha";
+        }
+        elseif( $datos['tipo']=='3' ){
+        $sql="  SELECT p.id, s.nombre AS sede, tp.nombre AS tipo_problema,
+                p.descripcion, ep.nombre AS estado_problema, 
+                p.fecha_problema,
+                p.created_at AS fecha_registro,
+                pd.fecha_estado, pd.resultado,a.nombre articulo,ap.descripcion art_descripcion,
+                ap.cantidad art_cantidad,ta.nombre tipo_articulo,
+                DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
+                FROM problemas p
+                JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
+                JOIN sedes s ON p.sede_id=s.id
+                JOIN problema_detalle pd ON p.id=pd.problema_id
+                JOIN (SELECT MAX(id) AS id
+                            FROM problema_detalle
+                            WHERE estado=1
+                            GROUP BY problema_id) pd2
+                ON pd.id=pd2.id
+                JOIN estado_problema ep ON pd.estado_problema_id=ep.id
+                LEFT JOIN articulo_problema ap ON p.id=ap.problema_id
+                LEFT JOIN articulos a ON a.id=ap.articulo_id
+                LEFT JOIN tipo_articulo ta ON ta.id=a.tipo_articulo
+                WHERE p.estado=1 AND pd.estado=1 
+                AND p.sede_id in ($sede)
+                $tipo
+                $estado $fecha";
+        }
+        elseif( $datos['tipo']=='2' ){
+        $sql="  SELECT p.id, s.nombre AS sede, tp.nombre AS tipo_problema,
+                descripcion, ep.nombre AS estado_problema, 
+                p.fecha_problema,
+                p.created_at AS fecha_registro,
+                pd.fecha_estado, pd.resultado,
+                DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
+                FROM problemas p
+                JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
+                JOIN sedes s ON p.sede_id=s.id
+                JOIN problema_detalle pd ON p.id=pd.problema_id
+                JOIN (SELECT MAX(id) AS id
+                            FROM problema_detalle
+                            WHERE estado=1
+                            GROUP BY problema_id) pd2
+                ON pd.id=pd2.id
+                JOIN estado_problema ep ON pd.estado_problema_id=ep.id
+                WHERE p.estado=1 AND pd.estado=1 
+                AND p.sede_id in ($sede)
+                $tipo
+                $estado $fecha";
+        }
+        elseif( $datos['tipo']=='1' ){
+        $sql="  SELECT p.id, s.nombre AS sede, tp.nombre AS tipo_problema,
+                descripcion, ep.nombre AS estado_problema, 
+                p.fecha_problema,
+                p.created_at AS fecha_registro,
+                pd.fecha_estado, pd.resultado,
+                DATEDIFF(CURDATE(),DATE(p.fecha_problema)) tiempo_transcurrido
                 FROM problemas p
                 JOIN tipo_problema tp ON p.tipo_problema_id=tp.id
                 JOIN sedes s ON p.sede_id=s.id
